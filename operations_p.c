fcd4e866b07e1c646eb411cb37c35e23f7e5359b
Derhen David <dedavid@c2r1p4.42lehavre.fr>
2025-11-02 14:23:35 +0100

    auto commit

--- operations_p.c
+++ operations_p.c
@@ -6,7 +6,7 @@
 /*   By: dedavid <dedavid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 14:10:59 by dedavid           #+#    #+#             */
-/*   Updated: 2025/11/02 14:12:51 by dedavid          ###   ########.fr       */
+/*   Updated: 2025/11/02 14:23:19 by dedavid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@ void	pusha(stacks_t stacks)
 {
 	if (stacks->sizeb == 0)
 		return ;
+	stacks->sizea += 1;
+	stacks->a[stacks->sizea] = stacks->b[stacks->sizeb];
+	stacks->b[stacks->sizeb] = -1;
+	stacks->bsize -= 1;
+}
 
+void	pushb(stacks_t stacks)
+{
+	if (stacks->sizea == 0)
+		return ;
+	stacks->sizeb += 1;
+	stacks->b[stacks->sizeb] = stacks->a[stacks->sizea];
+	stacks->a[stacks->sizea] = -1;
+	stacks->asize -= 1;
 }
