60d6623a9ce37023d5f3920434bcefe79f85b648
Derhen David <dedavid@c1r2p8.42lehavre.fr>
2025-11-03 14:41:51 +0100

    auto commit

--- solve.c
+++ solve.c
@@ -6,11 +6,25 @@
 /*   By: dedavid <dedavid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 16:27:59 by dedavid           #+#    #+#             */
-/*   Updated: 2025/11/02 16:37:36 by hkeromne         ###   ########.fr       */
+/*   Updated: 2025/11/03 14:41:48 by dedavid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.c"
+
+int	is_sorted(stacks_t stacks)
+{
+	int	i;
+
+	i = 0;
+	while (i < stacks->sizea)
+	{
+		if (stacks->a[i] > stacks->a[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 void	solve(stacks_t stacks)
 {
@@ -21,15 +35,39 @@ void	solve(stacks_t stacks)
 
 	lower = middle - 1;
 	higher = middle + 1;
-	while (1)
+	while (!is_sorted(stacks))
 	{
-		num = stacks->a[stacks->sizea] != middle 
+		num = stacks->a[stacks->sizea] != middle
 		if (num != lower || num != middle || num != middle)
+		{
 			pb(stacks);
+			continue ;
+		}
+		if (num > stacks->a[stacks->sizea - 1])
+			sa(stacks);
+		ra(stacks);
+	}
+	while (!is_sorted(stacks) && stacks->sizeb != 0)
+	{
+		index = get_closest(lower, higher);
+
+		way = stacks->sizeb - index < index;
+		while (stacks->b[0] != lower && stacks->b[0] != higher)
+		{
+			if (way)
+				rb(stacks);
+			else
+				rrb(stacks);
+		}
+		pa(stacks);
+		if (stacks->a[0] == higher)
+		{
+			rra(stacks);
+			higher++;
+		}
 		else
-		{	
-			if (num > stacks->a[stacks->sizea - 1])
-				sa(stacks);
+		{
+			lower++;
 		}
 	}
 }
